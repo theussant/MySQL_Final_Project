9eac78e105d5b5b418c879bffa8ff98b7fc26847
theus6 <msantossilva005@gmail.com>
2022-10-18 21:57:17 -0300

    views and procedures added

--- Data/petshopremake.sql
+++ Data/petshopremake.sql
@@ -97,7 +97,9 @@ dt_hr_venda datetime not null,
 desconto real not null,
 valor_total real not null,
 
+id_itens_venda int not null,
 id_func int not null,
+foreign key (id_itens_venda) references tb_itens_venda (id_itens_venda),
 foreign key (id_func) references tb_funcionario (id_func)
 );
 
@@ -160,11 +162,17 @@ insert into tb_produto (cod_produto, nome_produto, valor_custo, valor_venda, mar
 (4, "Perfume", 120.00, 20, "DuraPets", "Higiente", '2019-06-12', '2030-05-20', 20, 30, "53443421154511"),
 (5, "Coleira", 60.00, 20, "Clube Pet D&G", "Passeio", '2020-08-10', '2025-05-17', 10, 20, "98839923405422");
 
-# Inserção de dados itens venda
+# Inserção de dados itens venda.
 insert into tb_itens_venda (qtd_vendida, cod_produto) values
 (5, 1),
 (10, 2),
 (3, 3);
+
+# Inserção de dados da venda.
+insert into tb_venda (dt_hr_venda, desconto, valor_total, id_itens_venda, id_func) values
+('2022-09-11 15:10:35', 0, 250.00, 1, 1),
+('2021-02-01 12:20:45', 0, 400.00, 2, 1),
+('2022-01-10 11:00:05', 0, 240.00, 3, 2);
 
 # Selects para verificação das criações das tabelas.
 select * from tb_funcionario;
@@ -261,3 +269,58 @@ where cod_produto = old.cod_produto;
 end$
 
 DELIMITER ;
+
+# Procedures.
+DELIMITER $$
+create procedure pesquisar_animal(in idbusca int)
+begin
+select nome_animal, tipo_animal, raca, id_cliente
+from tb_animal
+where id_animal = idbusca;
+end $$
+DELIMITER ;
+
+DELIMITER $$
+create procedure pesquisar_funcionario(in idbusca int)
+begin
+select nome_func, cargo, salario, id_func
+from tb_funcionario
+where id_func = idbusca;
+end $$
+DELIMITER ;
+
+DELIMITER $$
+create procedure pesquisar_cliente(in idbusca int)
+begin
+select nome_cliente, endereco_cliente, contato_cliente, id_cliente
+from tb_cliente
+where id_cliente = idbusca;
+end $$
+DELIMITER ;
+
+DELIMITER $$
+create procedure selecionar_produto(in quantidade int)
+begin
+select *from tb_produto
+limit quantidade;
+end $$
+DELIMITER ;
+
+DELIMITER $$
+create procedure verificar_qtd_produto(out quantidade int)
+begin
+select count(*) into quantidade from tb_produto;
+end $$
+DELIMITER ;
+
+# Calls.
+call pesquisar_animal(2);
+call pesquisar_cliente(2);
+call pesquisar_funcionario(3);
+call selecionar_produto(3);
+call verificar_qtd_produto(@total);
+select @total;
+
+# Views.
+create view v as select qtd_vendida, valor_venda, qtd_vendida*valor_venda as Total from tb_itens_venda, tb_produto;
+select * from v;
